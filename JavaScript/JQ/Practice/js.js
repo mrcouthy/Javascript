b146a15ad5a0b6a4a66ec988b24c7b7d3106ea17
mrcouthy <mrcouthy@gmail.com>
2015-02-26 14:54:18 +0545

    Moving Javascript to a seperate repo

---
+++ JavaScript/JQ/Practice/js.js
@@ -0,0 +1,9 @@
+window.onload=function () {
+	var div = document.getElementById('one');
+	div.style.display = 'inline-block';
+	div.style.position = 'absolute';
+	div.style.left = '1100px';
+	div.style.top = '100px';
+}
+
+
